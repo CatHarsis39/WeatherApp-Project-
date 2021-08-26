f90a595e04426e6bbcda8d50572ab30086d8bd21
CatHarsis39 <88327812+CatHarsis39@users.noreply.github.com>
2021-08-26 11:20:02 +0100

    Took off text that is updated with real time data (date and hour)

---
+++ script/index.js
@@ -0,0 +1,94 @@
+// date
+
+function formatDate() {
+  let currentTime = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let dayIndex = days[currentTime.getDay()];
+  let currentMonth = months[currentTime.getMonth()];
+  let currentDate = currentTime.getDate();
+  let currentYear = currentTime.getFullYear();
+  let currentHour = currentTime.getHours();
+  let currentMinutes = currentTime.getMinutes();
+
+  let sentenceDate = `Today | ${dayIndex} ${currentDate}, ${currentMonth} ${currentYear}`;
+
+  let changeTodayDate = document.querySelector("#today-date");
+  changeTodayDate.innerHTML = sentenceDate;
+
+  let changeUpdate = document.querySelector("#last-update");
+  changeUpdate.innerHTML = `Last updated ${currentHour}: ${currentMinutes}`;
+}
+
+formatDate();
+
+// Weather Info main
+
+function showTemperatureInHtml(response) {
+  document.querySelector("h2").innerHTML = response.data.name;
+  document.querySelector("#temp-num").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#weather").innerHTML = response.data.weather[0].main;
+}
+
+function getApiInfo(city) {
+  let key = "9da2f08caa2513afeb23e54674daaf04";
+  let endPoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(`${endPoint}&appid=${key}`).then(showTemperatureInHtml);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value; // from input in html
+
+  getApiInfo(city);
+}
+
+let searchedCity = document.querySelector("#city-form");
+searchedCity.addEventListener("submit", handleSubmit);
+
+getApiInfo("New York"); // default city when page is loading
+
+// geolocation
+
+function searchLocation(position) {
+  let key = "9da2f08caa2513afeb23e54674daaf04";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${key}&units=metric`;
+  axios.get(apiUrl).then(showTemperatureInHtml);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
